1830cad3a8d3078b30cda4a0dcb5364850c6f35b
Muhammad Faizan <faizansohail077@gmail.com>
2020-12-18 03:46:46 -0500

    12c project netlify authentication

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Container, Heading, Button, Flex } from 'theme-ui'
+import netlifyIdentity from 'netlify-identity-widget'
 
-function props() {
+function Index() {
+    useEffect(()=>{
+        netlifyIdentity.init({})
+    })
     return (
-        <div>
-            <h1>Todo List Jamstack by faizan</h1>
-        </div>
+        <Container>
+            <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+                <Heading sx={{marginTop:5}}>Todo List Jamstack by faizan</Heading>
+                <Button sx={{marginTop:2}} onClick={()=>netlifyIdentity.open()}>Login</Button>
+            </Flex>
+        </Container>
     )
 }
 
-export default props
+export default Index
--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -2,7 +2,13 @@ const React = require('react')
 const { ThemeProvider } = require('theme-ui')
 const { dark } = require('@theme-ui/presets')
 
+const newTheme = {
+    ...dark,
+    sizes:{
+        container:1024,
+    }
+}
 
 module.exports =({ element }) => (
-    <ThemeProvider theme={dark}>{element}</ThemeProvider>
+    <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
 )
