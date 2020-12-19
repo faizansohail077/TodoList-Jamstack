27afed1ee7f4e04150f7e184cc1ff424852ef8e3
Muhammad Faizan <faizansohail077@gmail.com>
2020-12-19 01:12:29 -0500

    Project 12C

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Heading, Button, Flex,NavLink } from 'theme-ui'
+import { Container, Heading, Button, Flex, NavLink } from 'theme-ui'
 import netlifyIdentity from 'netlify-identity-widget'
 import { Link } from 'gatsby'
 
 function Index() {
-    const [user,setUser] = useState('');
+    const [user, setUser] = useState('');
     useEffect(() => {
         netlifyIdentity.init({})
     });
 
-    netlifyIdentity.on('login',user1 =>{
+    netlifyIdentity.on('login', user1 => {
         netlifyIdentity.close();
         setUser(user1)
     })
 
-    netlifyIdentity.on('logout',()=>{
+    netlifyIdentity.on('logout', () => {
         netlifyIdentity.close();
         setUser()
     })
@@ -26,18 +26,20 @@ function Index() {
                     Home
                   </NavLink>
                 <NavLink as={Link} to='/app' p={2}>
-                   DashBoard
+                    DashBoard
                 </NavLink>
-                
-                {user  && (<NavLink sx={{marginLeft:'500px'}}  p={2}>
-                   {<h1>Username</h1>}
-                   {<h2>{user.user_metadata.full_name}</h2>}
+
+                {user && (<NavLink sx={{ marginLeft: '500px' }} p={2}>
+                    {<h1>Username</h1>}
+                    {<h2>{user.user_metadata.full_name}</h2>}
                 </NavLink>)}
             </Flex>
 
             <Flex sx={{ flexDirection: 'column', padding: 3 }}>
                 <Heading sx={{ marginTop: 5 }}>Todo List Jamstack by faizan</Heading>
                 <Button sx={{ marginTop: 2 }} onClick={() => netlifyIdentity.open()}>Login</Button>
+                <Button sx={{ marginTop: 2 }} onClick={() => netlifyIdentity.currentUser()}>user</Button>
+
             </Flex>
         </Container>
     )
